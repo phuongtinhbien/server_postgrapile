e8eb339447fab5b9d1df06966e0d84937a26ae8f
phuong vu <phuongtinhbien12@gmail.com>
2018-11-25 01:07:33 +0700

    fix err

--- provision_2.sql
+++ provision_2.sql
@@ -649,8 +649,14 @@ $service_tmp$ LANGUAGE plpgsql;
 CREATE TRIGGER update_trigger AFTER UPDATE ON service_type
 FOR EACH ROW EXECUTE PROCEDURE updateServiceBranch();
 
-CREATE TRIGGER update_branch_trigger AFTER UPDATE ON branch
-FOR EACH ROW EXECUTE PROCEDURE updateServiceBranch();
+CREATE OR REPLACE FUNCTION updatebranchServiceBranch() RETURNS TRIGGER AS $branch_tmp$
+   BEGIN
+     update service_type_branch set status = NEW.status where branch_id = NEW.id;
+      RETURN NEW;
+   END;
+$branch_tmp$ LANGUAGE plpgsql;
 
+CREATE TRIGGER update_branch_trigger AFTER UPDATE ON branch
+FOR EACH ROW EXECUTE PROCEDURE updatebranchServiceBranch();
 
 
